3a5169929d8079326355358d8ed24997cad33b8a
peter.froberg@stenhamra.se <jtb10lNHVJ8>
2020-02-09 19:38:49 +0100

    börjar spåna

--- src/com/company/Bank.java
+++ src/com/company/Bank.java
@@ -1,4 +1,4 @@
-// Peter Idestam-Almquist, 2020-01-31.
+//Peter Fröberg, pefr7147
 
 package com.company;
 
@@ -8,22 +8,50 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 class Bank {
     // Instance variables.
-    //private final List<Account> accounts = new ArrayList<Account>();
+    /***
+     * List is changed to ConcurrentHashMap to better support multi threading
+     */
     private final ConcurrentHashMap<Integer, Account> accounts = new ConcurrentHashMap<>();
 
     /***
-     * Creates a concurentHashMap to store all account locks
-      */
+     * Creates a ConcurrentHashMap to store all account locks
+     */
     private ConcurrentHashMap<Integer, ReentrantReadWriteLock> accountlocks = new ConcurrentHashMap<>();
-
+    /***
+     * Random added for sleep function to be randomized and not all threads retries at the same time. The Random
+     * variable is used for thread sleep functions
+     */
 
     Random random = new Random();
+    private Object newAccountLock = new Object();
+    private int highestAccountNumber = 0;
+
     // Instance methods.
 
+    /***
+     * Method to generate accountNumbers that is thread safe to avoid 2 accounts with the same accountNumber
+     * @return - New accountNumber
+     */
+    int getNewAccountNumber(){
+        synchronized (newAccountLock) {
+            highestAccountNumber ++;
+            return highestAccountNumber;
+        }
+    }
+
+    /***
+     * Changed so it use the thread safe method getNewAccountNumber to generate the new account number
+     *
+     * @param balance - Balance to set on new account
+     * @return - accountNumber for the new account
+     */
     int newAccount(int balance) {
         int accountId;
-        accountId = accounts.size(); // FIX ORIGINAL
+            accountId = getNewAccountNumber();
         accounts.put(accountId, new Account(accountId, balance));
+        /***
+         * add a lock for every account to accountLocks, The key is the accountId and a reentrantReadWriteLock as value
+         */
         accountlocks.put(accountId, new ReentrantReadWriteLock());
         return accountId;
     }
@@ -31,13 +59,15 @@ class Bank {
     /***
      * getAccountBalance(int accountId)
      * @param accountId - recive which account to work with
-     * @return - Returns the balanace of the account but if the needed lock not is acquired with 100 attempts reurns null
-     * This is not a safe return value as the account theoretically can have the balance of -999999999
+     * @return - Returns the balanced of the account but if the needed lock not is acquired with 100 attempts returns null
+     * This is not a safe return value as the account theoretically can have the balance of -999999999, the return type could be changed
+     * to Integer and then return null if after 100 retries no lock was acquired
      *
      *  Have been modified to lock the account with an ReentrantReadWriteLock in read mode. To allow multiple threads to read the
-     *  value
+     *   at the same time, but wait if any thread have a writelock
      *
      *  If the thread is unable to acquire the lock it retries 100 times with a random sleep of 1-100 ms between the tries
+     *  to make sure there is no deadlocks.
      */
     int getAccountBalance(int accountId) {
         Account account = null;
@@ -46,7 +76,7 @@ class Bank {
             if (accountlocks.get(account.getId()).readLock().tryLock()) {
                 try {
                     return account.getBalance();
-                }finally {
+                } finally {
                     accountlocks.get(account.getId()).readLock().unlock();
                 }
             } else {
@@ -82,9 +112,9 @@ class Bank {
      * @param operation - which operation that shold be run
      *
      *                  Tries 100 times to acquire the lock for the specified account
-     *                  in the first try statment the operation is performed and in the finally statment the lock is released
+     *                  in the first try statement the operation is performed and in the finally statement the lock is released
      *                  The try stament ensures that the look i release even if an exception is thrown when performing the operation
-     *                  in the else stament it makes the thread go to sleep in 1-100 ms by using the Random function if the look was not acquired
+     *                  in the else statement it makes the thread go to sleep in 1-100 ms by using the Random function if the look was not acquired
      */
     void runOperation(Operation operation) {
         Account account = null;
@@ -116,7 +146,7 @@ class Bank {
     /***
      *
      * @param operations - a List of operations to work on
-     * @return  Returns true-if all locks was acquired, false-if not all locks was acquired
+     * @return Returns true-if all locks was acquired, false-if not all locks was acquired
      *
      * Tries to acquire the locks for all the lock the account in the list of operations
      */
@@ -133,7 +163,7 @@ class Bank {
      *
      * @param operations - a list of operations to work on
      *
-     *                   relaese the lock for the accouts in the list of operations held by this thread
+     *                   release the lock for the accounts in the list of operations held by this thread
      *
      */
     private void releaseCurrentLockOperations(List<Operation> operations) {
@@ -146,13 +176,21 @@ class Bank {
     /***
      *
      * @param transaction a transaction to work on
-     *                    loopd through all operations in the transaction sent.First starts to loop all operations in the transaction
-     *                    secondly starts a loop for retries if not all
-     *                    account locks is acquired, it make 100 tries to acquire the locks
-     *                    first if-statment tries to acquire all the account locks for all operations in the transaction
-     *                    if not all locks are acquired the thread sleep for 1-100ms (random by Random function) and retries to acquire the locks again
-     *                    if all locks was acquired, enters the try stament to perform the operations.
-     *                    when all the operations is done release all the lock in the finally statment
+     *                    loop through all operations in the transaction sent to the method.
+     *                    First loop starts to loop all operations in the transaction
+     *                    second loop  starts a loop for retries if not all account locks is acquired, it make 100 tries to acquire the locks
+     *                    first if-statement tries to acquire all the account locks for all operations in the transaction
+
+     *                    if all locks was acquired, it enters the try statement to perform the operations in the transaction.
+     *                    when all the operations is done it release all the lock in the finally statement. The break statement
+     *                    makes the program to exit the for-loop when all operations is performed
+     *
+     *                    if not all locks are acquired, all locks are released and the thread sleep for 1-100ms (random by Random function)
+     *                    and retries to acquire the locks again, this is to avoid deadlocks. To make sure that all operations in one transaction
+     *                    is performed it is required to acquire all locks before any operations is performed
+     *
+     *                    I have chosen this method to ensure that it is possible to do all operations in a row to ensure consistent account data
+     *
      */
     void runTransaction(Transaction transaction) {
         List<Operation> currentOperations = transaction.getOperations();
@@ -176,7 +214,6 @@ class Bank {
                 }
             }
         }
-
     }
 
     public ConcurrentHashMap<Integer, Account> getAccounts() {
--- src/com/company/Program.java
+++ src/com/company/Program.java
@@ -7,8 +7,8 @@ import java.util.concurrent.TimeUnit;
 
 public class Program {
     // Static variables.
-    private static final int NUM_THREADS = 4;
-    private static final int NUM_ACCOUNTS = 80000;
+    private static final int NUM_THREADS = 8;
+    private static final int NUM_ACCOUNTS = 10000;
     private static final int FACTOR = 200;
     private static final int TIMEOUT = 60; // Seconds;
     private static final int NUM_TRANSACTIONS = NUM_ACCOUNTS * FACTOR;
